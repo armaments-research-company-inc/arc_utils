91aed40f01334e9276d0281dd8cbf329a23a7062
Minhaj Uddin Khan <minhajuddin.khan@tenpearls.com>
2019-11-14 15:33:16 +0500

    Restructure folders

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ import setuptools
 setuptools.setup(
     name="arc_utils",
     version="0.0.1",
+    url='https://github.com/armaments-research-company-inc/arc_utils',
     author="Minhaj Uddin Khan",
     author_email="minhajuddin.khan@@tenpearls.com",
     description="Utility methods for ARC",
     python_requires='>=3.6',
+    packages=['arc_utils'],   
 )

5dc5268c391ca08237e2faf8a8db01b165c54b33
faizanahmed-10p <faizan.ahmed@tenpearls.com>
2019-11-18 11:52:29 +0500

    time stamp corrected

--- arc_utils/date.py
+++ arc_utils/date.py
@@ -1,19 +1,28 @@
 # date.py
 
 import time
-from datetime import datetime
+from datetime import datetime,timedelta
 
 class DateUtils():
 
-    def __init__(self, refTime=datetime(2016, 1, 1, 0, 0, 0)):
-        self.refTime = refTime
+    def __init__(self, updatedRefTime=datetime(2016, 1, 1, 0, 0, 0),
+                         defaultRefTime=datetime(1970, 1, 1, 0, 0, 0)):
+        self.updatedRefTime = updatedRefTime
+        self.defaultRefTime = defaultRefTime
         self.milliseconds = 1000
 
     def elapsed_time(self) -> int:
         now = datetime.utcnow()
-        elapsed = (now - self.refTime).total_seconds()
-        return elapsed * self.milliseconds
-
+        elapsed = (now - self.updatedRefTime).total_seconds()
+        return int(elapsed * self.milliseconds)
 
     def elapsed_from(self, time_in_ms) -> int:
-        return self.elapsed_time() - time_in_ms
+        return int(self.elapsed_time() - time_in_ms)
+
+
+    def calculate_no_of_days(self, updated_time_stamp, previous_timestamp):
+        return str(updated_time_stamp - previous_timestamp).split(' ')[0]
+    
+    def calculate_timestamp(self, device_timestamp):
+        calculated_datetime = (datetime.fromtimestamp(float(device_timestamp) / 1000.)) + timedelta (days = int(self.calculate_no_of_days(self.updatedRefTime, self.defaultRefTime)))
+        return calculated_datetime

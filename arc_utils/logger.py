4723a066a0952c40717b30a2aedcf8c5a5c67119
faizanahmed-10p <faizan.ahmed@tenpearls.com>
2020-01-20 15:37:54 +0500

    logger and log rotation added

---
+++ arc_utils/logger.py
@@ -0,0 +1,28 @@
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+
+class Logger():
+    def __init__(self,file_path):
+        self.logger = logging.getLogger("Rotating Log")
+        self.logger.setLevel(logging.INFO)
+        formatter=logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        handler = TimedRotatingFileHandler(file_path, #log file path
+                                        when="h", # h = hour
+                                        interval=1, #rotate file after every hour
+                                        backupCount=48)  #overwite first file after 48 hours (interval * 48)
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+    
+    def getLogger(self):
+        return self.logger
+
+    # def info(self,msg):
+    #     self.logger.info(msg)
+
+    # def warn(self,msg):
+    #     self.logger.warn(msg)
+
+    # def error(self,msg):
+    #     self.logger.error(msg)
+

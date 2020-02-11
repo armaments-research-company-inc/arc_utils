bd7e851e8a7772be2d7786df09a73a8e8d15466b
zulfiqar10p <muhammad.zulfiqar@tenpearls.com>
2020-02-11 16:55:09 +0500

    updated version number

--- arc_utils/logger.py
+++ arc_utils/logger.py
@@ -3,16 +3,16 @@ from logging.handlers import TimedRotatingFileHandler
 
 
 class Logger():
-    def __init__(self,file_path,when="h",interval=1,backupCount=48):
-        self.logger = logging.getLogger(__name__)
+    def __init__(self, file_path, when="h", interval=1, backupCount=48, key=__name__):
+        self.logger = logging.getLogger(key)
         self.logger.setLevel(logging.INFO)
-        formatter=logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s')
-        handler = TimedRotatingFileHandler(file_path, #log file path
-                                        when=when, # h = hour
-                                        interval=interval, #rotate file after every hour
-                                        backupCount=backupCount)  #overwrite first file after 48 hours (interval * 48)
+        formatter = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s')
+        handler = TimedRotatingFileHandler(file_path,  # log file path
+                                           when=when,  # h = hour
+                                           interval=interval,  # rotate file after every hour
+                                           backupCount=backupCount)  # overwrite first file after 48 hours (interval * 48)
         handler.setFormatter(formatter)
         self.logger.addHandler(handler)
-    
+
     def getLogger(self):
         return self.logger
--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 setuptools.setup(
     name="arc_utils",
-    version="0.0.4",
+    version="0.0.7",
     url='https://github.com/armaments-research-company-inc/arc_utils',
     author="Minhaj Uddin Khan",
     author_email="minhajuddin.khan@tenpearls.com",
     description="Utility methods for ARC",
     python_requires='>=3.6',
-    packages=['arc_utils'],   
+    packages=['arc_utils'],
 )

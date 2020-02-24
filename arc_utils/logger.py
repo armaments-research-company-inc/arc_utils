ed7e94307792f600b9c9667b1e8b01a18a3bcf90
faizanahmed-10p <faizan.ahmed@tenpearls.com>
2020-02-24 16:08:19 +0500

    json format added for logger and kafka connect changes in producer

--- arc_utils/logger.py
+++ arc_utils/logger.py
@@ -6,7 +6,7 @@ class Logger():
     def __init__(self,file_path,when="h",interval=1,backupCount=48,key=__name__):
         self.logger = logging.getLogger(key)
         self.logger.setLevel(logging.INFO)
-        formatter=logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s')
+        formatter=logging.Formatter('{ "time" : "%(asctime)s" , "level" : "%(levelname)s" ,  "message" : "%(message)s"}')
         handler = TimedRotatingFileHandler(file_path, #log file path
                                         when=when, # h = hour
                                         interval=interval, #rotate file after every hour

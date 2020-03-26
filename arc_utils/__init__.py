b02ebcbfb873a588935db8c55ad441de04f1c1b8
Zawar Ahmed Tahir <faizan.ahmed@tenpearls.com>
2020-03-26 12:55:44 +0500

    package_dir added

--- arc_utils/__init__.py
+++ arc_utils/__init__.py
@@ -1,4 +1,4 @@
 # __init__.py
 # from .date import DateUtils
 # from .producer import KafkaLogProducer
-from arc_utils.cache_factory.factory import CacheFactory, Caches
+# from arc_utils.cache_factory.factory import CacheFactory, Caches

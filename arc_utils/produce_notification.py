3b50dbab8b9dc2fdcb537f2ee18bfa215970f37f
Zawar Ahmed Tahir <faizan.ahmed@tenpearls.com>
2020-03-30 14:30:42 +0500

    suggested changes made

--- arc_utils/produce_notification.py
+++ arc_utils/produce_notification.py
@@ -1,23 +1,41 @@
 # producer.py
 
 import json
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
 
 from kafka import KafkaProducer
+from kafka import KafkaProducer
+from schema import Schema
 
 
+
+login_schema = Schema({
+                "type": "LOGIN_SUCCESSFUL",
+                "payload":
+                {
+                    "user_email": str,
+                    "app_name": str
+                }
+                })
 class Notification:
-    def __init__(self):
+    def __init__(self,broker_url):
+        self.schema_list={"LOGIN_SUCCESSFUL":login_schema}
         self.topic = "arc-rte-notifications"
-        broker_url = ["arc-kafka.pt-staging.com:9092"]
+        
         self.__producer = KafkaProducer(
-            bootstrap_servers=broker_url,
+            bootstrap_servers=self.get_kafka_brokers(broker_url),
             value_serializer=lambda v: json.dumps(v).encode("utf-8"),
         )
-
+    def get_kafka_brokers(self, brokers):
+        return [
+            "{}:{}".format(parsedUrl.hostname, parsedUrl.port)
+            for parsedUrl in [urlparse(url) for url in brokers]
+        ]
     def send_notification(self, notification_type, payload):
-        if notification_type == "LOGIN_SUCCESSFUL": 
+        if self.schema_list[notification_type].validate(payload): 
             self.__producer.send(self.topic, payload)
             self.__producer.flush()
-            return True
-        else:
-            return False
+        

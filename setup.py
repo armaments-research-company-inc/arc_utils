03b420afe1ecd530d442491f90c86733c9cccebe
Zawar Ahmed Tahir <faizan.ahmed@tenpearls.com>
2020-03-30 18:22:13 +0500

    notification throttle

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from setuptools import find_packages
 setuptools.setup(
     name="arc_utils",
-    version="0.3.1",
+    version="0.3.2",
     url='https://github.com/armaments-research-company-inc/arc_utils',
     author="Minhaj Uddin Khan",
     author_email="minhajuddin.khan@tenpearls.com",
